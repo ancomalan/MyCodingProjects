86dece15944b740fb110914bd1c7b4090ba5d802
Alan Vo <124543342+ancomalan@users.noreply.github.com>
2023-02-07 17:02:41 -0600

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,8 +0,0 @@
-
-public class HelloWorld {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Java!");
-		System.out.println("My name is Alan!");
-		System.out.println("And I think that I am handsome!");
-	}
-}
