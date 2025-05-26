e275e694a35f8e2e940a002223632562899b8cf3
Alan Vo <bvo101968@gmail.com>
2025-05-26 15:37:43 -0500

    final solution

--- hashset.cpp
+++ hashset.cpp
@@ -1,20 +1,28 @@
-//leetcode problem: designing hashset
+//leetcode problem 705: designing hashset
 class MyHashSet {
 public:
+
+    bool keys[1000001]; //array tracks existence of keys
     MyHashSet() {
-        
+        //initialize all keys to false
+        for (int i = 0; i < 1000001; i++) {
+            keys[i] = false;
+        }
     }
     
+    //inserts key into hashset
     void add(int key) {
-        
+        keys[key] = true;
     }
     
+    //removes key from hashset
     void remove(int key) {
-        
+        keys[key] = false;
     }
     
+    //returns whether key exists in hashset or not
     bool contains(int key) {
-        
+        return keys[key];
     }
 };
 
